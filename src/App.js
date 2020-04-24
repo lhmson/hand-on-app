ca92a31f1ed7a22db02a189f64d3f1f0c8dfdab3
lhmson <lehoangminhson123@gmail.com>
2020-04-24 14:20:48 +0700

    Import dependencies of tfjs and howler
    get sound play and add essential patterns of tensorflow and howler

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
 import React from 'react';
+// import {Howl} from 'howler';
+// import * as mobilenet from '@tensorflow-models/mobilenet';
+// import * as knnClassifier from '@tensorflow-models/knn-classifier';
 import './App.css';
+// import soundURL from './assets/noti_out.mp3';
+
+// var sound = new Howl({
+//   src: [soundURL]
+// });
+// sound.play();
 
 function App() {
   return (
