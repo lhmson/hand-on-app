b9071373a63a975d192b7d351151d1374a35dbf6
lhmson <lehoangminhson123@gmail.com>
2020-04-24 13:47:51 +0700

    Make changes in UI and setup dependencies
    check the files App.js, index.css, app.css and write own code
    go to github of tensorflow (tfjs-models) and copy npm install of mobilenet and knn
    go to howler sound library github and copy install npm howler
    get sound in assets
    make change in App.js and App.css to form basic website

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <h1>HELLO TO MY APP</h1>
+      <video
+        className="video"
+        autoPlay
+      />
+      <div className="control">
+        <button className="btn">Train 1</button>
+        <button className="btn">Train 2</button>
+        <button className="btn">Start</button>
+      </div>
     </div>
   );
 }
