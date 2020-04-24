99a1d49fc02b29c5ec5e0b697bea6a0751fcc64c
lhmson <lehoangminhson123@gmail.com>
2020-04-24 15:02:04 +0700

    Setup camera functions
    Add video, init, setupCamera, useEffect into App and turn on

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 // import {Howl} from 'howler';
 // import * as mobilenet from '@tensorflow-models/mobilenet';
 // import * as knnClassifier from '@tensorflow-models/knn-classifier';
@@ -11,10 +11,50 @@ import './App.css';
 // sound.play();
 
 function App() {
+
+  const video = useRef();
+  
+  const init = async () => {
+    console.log('init');
+    await setupCamera();
+    console.log("setup camera enter")
+  }
+  // ask to access user camera
+  const setupCamera = () => {
+    return new Promise((resolve,reject) => {
+      navigator.getUserMedia = navigator.getUserMedia ||
+        navigator.webkitGetUserMedia ||
+        navigator.mozGetUserMedia ||
+        navigator.msGetUserMedia;
+      
+      if(navigator.getUserMedia){
+        navigator.getUserMedia(
+          { video: true },
+          stream => {
+            video.current.srcObject = stream;
+            video.current.addEventListener('loadeddata', resolve)
+          },
+          error => reject(error)
+        );
+      }
+      else {
+        reject();
+      }
+    });
+  }
+  useEffect(() => {
+    init();
+    console.log("it runs")
+    // cleanup
+    return () => {
+
+    }
+  }, []);
   return (
     <div className="main">
       <h1>HELLO TO MY APP</h1>
       <video
+        ref={video}
         className="video"
         autoPlay
       />
